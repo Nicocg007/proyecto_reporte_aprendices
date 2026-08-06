808f29286b732b8b208b8a6a7dd1713144f05787
jhonsebastian1753-linux <jhonsebastian1753@gmail.com>
2026-08-06 08:25:54 -0500

    base index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro expedita animi obcaecati nisi aliquid accusantium magni dolore, quam nesciunt laudantium voluptatibus. Perspiciatis sit assumenda corporis velit aut placeat fugiat! Expedita.</h1>
+</body>
+</html>
